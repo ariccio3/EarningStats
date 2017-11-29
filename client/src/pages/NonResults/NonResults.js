fdee628b330a26b0cf2c0f51cbfb69cf2249dccb
ariccio3 <ar3home@gmail.com>
2017-11-28 19:05:06 -0800

    Quarterly done

--- client/src/pages/NonResults/NonResults.js
+++ client/src/pages/NonResults/NonResults.js
@@ -137,7 +137,6 @@ class NonResults extends Component {
         count++;
       } 
     } 
-    console.log(count);
     return count;
   };
 
@@ -148,8 +147,167 @@ class NonResults extends Component {
         count++;
       } 
     } 
-    console.log(count);
     return count;
+  };
+  //***
+  stockOutsideNumUp = () => {
+    let count = 0;
+    for (let i = 0; i < this.state.stocks.length; i++) {
+      if (this.state.stocks[i].insideOutside === 'OUTSIDE' && this.state.stocks[i].actualMove >= 0) {
+        count++;
+      } 
+    } 
+    return count;
+  };
+
+  watchListOutsideNumUp = () => {
+    let count = 0;
+    for (let i = 0; i < this.state.watchList.length; i++) {
+      if (this.state.watchList[i].insideOutside === 'OUTSIDE' && this.state.watchList[i].actualMove >= 0) {
+        count++;
+      } 
+    } 
+    return count;
+  };
+  //***
+  stockOutsideNumDown = () => {
+    let count = 0;
+    for (let i = 0; i < this.state.stocks.length; i++) {
+      if (this.state.stocks[i].insideOutside === 'OUTSIDE' && this.state.stocks[i].actualMove < 0) {
+        count++;
+      } 
+    } 
+    return count;
+  };
+
+  watchListOutsideNumDown = () => {
+    let count = 0;
+    for (let i = 0; i < this.state.watchList.length; i++) {
+      if (this.state.watchList[i].insideOutside === 'OUTSIDE' && this.state.watchList[i].actualMove < 0) {
+        count++;
+      } 
+    } 
+    return count;
+  };
+  //***
+  stockQone = () => {
+    let insideCount = 0;
+    let quarterCount = 0;
+    for (let i = 0; i < this.state.stocks.length; i++) {
+      if (this.state.stocks[i].insideOutside === 'INSIDE' && this.state.stocks[i].quarter === 'Q1') {
+        insideCount++;
+      }
+      if (this.state.stocks[i].quarter === 'Q1') {
+        quarterCount++;
+      } 
+    } 
+    console.log(insideCount);
+    return insideCount / quarterCount *100;
+  };
+
+  watchListQone = () => {
+    let insideCount = 0;
+    let quarterCount = 0;
+    for (let i = 0; i < this.state.watchList.length; i++) {
+      if (this.state.watchList[i].insideOutside === 'INSIDE' && this.state.watchList[i].quarter === 'Q1') {
+        insideCount++;
+      }
+      if (this.state.watchList[i].quarter === 'Q1') {
+        quarterCount++;
+      } 
+    } 
+    console.log(insideCount);
+    return insideCount / quarterCount *100;
+  };
+  //***
+  stockQtwo = () => {
+    let insideCount = 0;
+    let quarterCount = 0;
+    for (let i = 0; i < this.state.stocks.length; i++) {
+      if (this.state.stocks[i].insideOutside === 'INSIDE' && this.state.stocks[i].quarter === 'Q2') {
+        insideCount++;
+      }
+      if (this.state.stocks[i].quarter === 'Q2') {
+        quarterCount++;
+      } 
+    } 
+    console.log(insideCount);
+    return insideCount / quarterCount *100;
+  };
+
+  watchListQtwo = () => {
+    let insideCount = 0;
+    let quarterCount = 0;
+    for (let i = 0; i < this.state.watchList.length; i++) {
+      if (this.state.watchList[i].insideOutside === 'INSIDE' && this.state.watchList[i].quarter === 'Q2') {
+        insideCount++;
+      }
+      if (this.state.watchList[i].quarter === 'Q2') {
+        quarterCount++;
+      } 
+    } 
+    console.log(insideCount);
+    return insideCount / quarterCount *100;
+  };
+  //***
+    stockQthree = () => {
+    let insideCount = 0;
+    let quarterCount = 0;
+    for (let i = 0; i < this.state.stocks.length; i++) {
+      if (this.state.stocks[i].insideOutside === 'INSIDE' && this.state.stocks[i].quarter === 'Q3') {
+        insideCount++;
+      }
+      if (this.state.stocks[i].quarter === 'Q3') {
+        quarterCount++;
+      } 
+    } 
+    console.log(insideCount);
+    return insideCount / quarterCount *100;
+  };
+
+  watchListQthree = () => {
+    let insideCount = 0;
+    let quarterCount = 0;
+    for (let i = 0; i < this.state.watchList.length; i++) {
+      if (this.state.watchList[i].insideOutside === 'INSIDE' && this.state.watchList[i].quarter === 'Q3') {
+        insideCount++;
+      }
+      if (this.state.watchList[i].quarter === 'Q3') {
+        quarterCount++;
+      } 
+    } 
+    console.log(insideCount);
+    return insideCount / quarterCount *100;
+  };
+  //***
+    stockQfour = () => {
+    let insideCount = 0;
+    let quarterCount = 0;
+    for (let i = 0; i < this.state.stocks.length; i++) {
+      if (this.state.stocks[i].insideOutside === 'INSIDE' && this.state.stocks[i].quarter === 'Q4') {
+        insideCount++;
+      }
+      if (this.state.stocks[i].quarter === 'Q4') {
+        quarterCount++;
+      } 
+    } 
+    console.log(insideCount);
+    return insideCount / quarterCount *100;
+  };
+
+  watchListQfour = () => {
+    let insideCount = 0;
+    let quarterCount = 0;
+    for (let i = 0; i < this.state.watchList.length; i++) {
+      if (this.state.watchList[i].insideOutside === 'INSIDE' && this.state.watchList[i].quarter === 'Q4') {
+        insideCount++;
+      }
+      if (this.state.watchList[i].quarter === 'Q4') {
+        quarterCount++;
+      } 
+    } 
+    console.log(insideCount);
+    return insideCount / quarterCount *100;
   };
   //***
 
@@ -230,23 +388,23 @@ class NonResults extends Component {
             </tr>
             <tr>
               <td># Up</td>
-              <td>{}</td>
-              <td>{}</td>
+              <td>{this.stockOutsideNumUp()}</td>
+              <td>{this.watchListOutsideNumUp()}</td>
             </tr>            
             <tr>
-              <td>% Up</td>
-              <td>{}</td>
-              <td>{}</td>
+              <td><strong>% Up</strong></td>
+              <td><strong>{Math.floor(this.stockOutsideNumUp() / this.state.stocks.length *100)}%</strong></td>
+              <td><strong>{Math.floor(this.watchListOutsideNumUp() / this.state.watchList.length *100)}%</strong></td>
             </tr>
             <tr>
               <td># Down</td>
-              <td>{}</td>
-              <td>{}</td>
+              <td>{this.stockOutsideNumDown()}</td>
+              <td>{this.watchListOutsideNumDown()}</td>
             </tr>            
             <tr>
-              <td>% Down</td>
-              <td>{}</td>
-              <td>{}</td>
+              <td><strong>% Down</strong></td>
+              <td><strong>{Math.floor(this.stockOutsideNumDown() / this.state.stocks.length *100)}%</strong></td>
+              <td><strong>{Math.floor(this.watchListOutsideNumDown() / this.state.watchList.length *100)}%</strong></td>
             </tr>            
           </tbody>
         </table><br/>                  
@@ -289,23 +447,23 @@ class NonResults extends Component {
           <tbody>
             <tr>
               <td>Q1 % Inside Expected Move</td>
-              <td>{this.state.stocks.length}</td>
-              <td>{this.state.watchList.length}</td>
+              <td>{Math.floor(this.stockQone())}%</td>
+              <td>{Math.floor(this.watchListQone())}%</td>
             </tr>
             <tr>
               <td>Q2 % Inside Expected Move</td>
-              <td>{this.stockTotalUp()}</td>
-              <td>{this.watchListTotalUp()}</td>
+              <td>{Math.floor(this.stockQtwo())}%</td>
+              <td>{Math.floor(this.watchListQtwo())}%</td>
             </tr>
             <tr>
               <td>Q3 % Inside Expected Move</td>
-              <td>{this.stockTotalDown()}</td>
-              <td>{this.watchListTotalDown()}</td>
+              <td>{Math.floor(this.stockQthree())}%</td>
+              <td>{Math.floor(this.watchListQthree())}%</td>
             </tr>
             <tr>
               <td>Q4 % Inside Expected Move</td>
-              <td>{this.stockTotalDown()}</td>
-              <td>{this.watchListTotalDown()}</td>
+              <td>{Math.floor(this.stockQfour())}%</td>
+              <td>{Math.floor(this.watchListQfour())}%</td>
             </tr>            
           </tbody>
         </table>
