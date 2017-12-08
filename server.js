935c5fba640c68f16522bf437e5c41026289e519
ariccio3 <ar3home@gmail.com>
2017-12-08 10:48:10 -0800

    MongoLab creds

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const logger = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
-app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
@@ -19,20 +17,11 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/earnings_data" || "mongodb://admin:admin@ds161194.mlab.com:61194/heroku_p9xxsj90",
+  process.env.MONGODB_URI || "mongodb://localhost/earnings_data" || "mongodb://heroku_bqh4kgg0:ksue5hj0s6vham9a6nmhbdom6r@ds133876.mlab.com:33876/heroku_bqh4kgg0",
     {
     	useMongoClient: true
   }
 );
-// var db = mongoose.connection;
-
-// db.on("error", function(error) {
-// 	console.log("Mongoose Error: ", error);
-// });
-
-// db.once("open", function() {
-// 	console.log("Mongoose connection successful.");
-// });
 
 // Start the API server
 app.listen(PORT, function() {
