0c517bd77d791e19a76c43ce87af09f36b4fd093
ariccio3 <ar3home@gmail.com>
2017-12-11 11:36:58 -0800

    User input conditional

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import API from "../../utils/API";
 // import {Alert} from "react-bootstrap";
 
 class Navbar extends Component {
   // Setting the component's initial state
   state = {
-    stocks: ""
+    stocks: "",
+    watchList: []
   }
+
+  componentDidMount() {
+    this.loadWatchList();
+  }
+
+  loadWatchList = () => {
+    API.getWatchList()
+    .then(res => this.setState({ watchList: res.data }))
+    .catch(err => console.log(err));
+  };  
+
+  watchList = () => {
+    let arr = [];
+    for (let i = 0; i < this.state.watchList.length; i++) {
+      if (arr.indexOf(this.state.watchList[i].stock) === -1){
+        arr.push(this.state.watchList[i].stock);
+      }
+    }  
+    let watchListArray = arr.reverse();
+    return watchListArray; 
+  }; 
 
   handleInputChange = event => {
     // Getting the value and name of the input which triggered the change
@@ -17,20 +40,28 @@ class Navbar extends Component {
     this.setState({
       [name]: value
     });
-  }
+  };
 
   handleFormSubmit = event => {
     event.preventDefault();
+    let uniqueStocks = this.watchList();
+    let userInput = this.state.stocks.toUpperCase();
 
     if (!this.state.stocks) {
       alert("Please enter a stock ticker you'd like to search for");
-    } else {
-      window.location.href = "/results/" + this.state.stocks.toUpperCase()
-    }
+    } 
+
+    if (userInput) {
+      if (uniqueStocks.indexOf(userInput) !== -1) {
+        window.location.href = "/results/" + this.state.stocks.toUpperCase();  
+      } else if (uniqueStocks.indexOf(userInput) === -1){
+        alert("Sorry that stock is not in the EarningStats watchlist"); 
+      }
+    } 
       this.setState({
         stocks: ""
       });
-  }
+  };
   
   render() {
     return (
