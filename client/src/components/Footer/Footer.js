f5260b225e0f9e5ebc6b18d7e5d26f929aa9f044
ariccio3 <ar3home@gmail.com>
2018-01-04 15:49:59 -0800

    Contact info added

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,21 +2,17 @@ import React from "react";
 import "./Footer.css";
 import PayPal from "../../components/PayPal";
 
-const contact = () => {
+
 	let username = "earningstats";
 	let hostname = "yahoo.com";
 	let linktext = username + "@" + hostname ;
 
-	return linktext;
-};
-
 const Footer = () =>
   <footer className="footer">
-    <span className="footerText"><b>EarningStats 2018&emsp;|&emsp;Contact: <a href="mailto:{contact()}">{contact()}</a></b></span>
+    <span className="footerText"><b>EarningStats 2018&emsp;|&emsp;Contact: <a href="mailto:{linktext}">{linktext}</a></b></span><br/>
     <div className="paypal">
         <PayPal />
     </div>
-    <hr/>
   </footer>;
 
 export default Footer;
