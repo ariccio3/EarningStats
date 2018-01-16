3e9c0222262c595f816760a606c7889d8c85bdd0
ariccio3 <ar3home@gmail.com>
2018-01-15 18:15:16 -0800

    Footer update

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () =>
   <footer className="footer">
-    <span className="footerText"><b>EarningStats 2018&emsp;|&emsp;Contact: <a href="mailto:earningstats@yahoo.com">earningstats@yahoo.com</a></b></span><br/>
+    <span className="footerText"><b>EarningStats 2003-2018&emsp;|&emsp;Contact: <a href="mailto:earningstats@yahoo.com">earningstats@yahoo.com</a></b></span><br/>
     <div className="paypal">
         <PayPal />
     </div>
