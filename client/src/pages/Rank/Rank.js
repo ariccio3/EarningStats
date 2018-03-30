3475eb1526376bf690177266163b4474e276eb55
ariccio3 <ar3home@gmail.com>
2018-03-29 18:01:15 -0700

    Img map test

--- client/src/pages/Rank/Rank.js
+++ client/src/pages/Rank/Rank.js
@@ -12,7 +12,14 @@ class Rank extends Component {
     return (
       <div className="container">
         <h3>EarningStats Predictability Rank<hr /></h3>
-        <img className="rankImg" src={require("./SM.JPG")} alt="EarningStats Predictability Rank" />
+        <img className="rankImg" src={require("./SM.JPG")} alt="EarningStats Predictability Rank" useMap="#stockmap" />
+          
+        <map name="stockmap">
+          <area shape="rect" coords="3,38,32,27" href="http://www.earningstats.com/results/CVX" alt="null" / >
+          <area shape="rect" coords="" href="" / >
+        
+        </map>
+
         <RankExp />
       </div>         
     );
