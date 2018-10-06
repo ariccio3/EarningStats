25e2ac8a3dfe4ebaab30dcfd442bd42daf961ca9
Anthony <ar3home@gmail.com>
2018-10-05 20:42:27 -0700

    Rank Article date update

--- client/src/components/RankExp/RankExp.js
+++ client/src/components/RankExp/RankExp.js
@@ -46,7 +46,7 @@ const RankExp = props => (
 	</p>
 	<p>	
 		 If you want to share how you use the EarningStats Predicatability Rank, please contact us by email or Twitter at the bottom of the page. We would love to hear from you. 
-		 This ranking will be updated 4 times per year at the end of each earnings season. It was last updated at the end of Q4 2017 earnings reports in March 2018. 
+		 This ranking will be updated 4 times per year at the end of each earnings season. It was last updated at the end of Q1 2018 earnings reports in March 2018. 
 	</p>
 	<h3><i>ENJOY!!</i></h3>
   </div>
