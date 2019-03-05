a2bf69256dd70a101c22006c3798817cbc0b7aa7
Anthony <ar3home@gmail.com>
2019-03-05 07:52:22 -0800

    update

--- client/src/pages/EarningsCalendar/EarningsCalendar.js
+++ client/src/pages/EarningsCalendar/EarningsCalendar.js
@@ -21,19 +21,7 @@ class EarningsCalendar extends Component {
               <th className="time">Time</th>
             </tr>
           </thead>
-          <tbody>     
-            <tr>
-              <td><Link to="/results/CRM">CRM</Link></td>
-              <td>SalesForce</td>
-              <td>3/4/2019</td>
-              <td>amc</td>
-            </tr>  
-            <tr>
-              <td><Link to="/results/TGT">TGT</Link></td>
-              <td>Target</td>
-              <td>3/5/2019</td>
-              <td>bmo</td>
-            </tr>               
+          <tbody>                  
             <tr>
               <td><Link to="/results/COST">COST</Link></td>
               <td>Costco</td>
@@ -367,6 +355,18 @@ class EarningsCalendar extends Component {
               <td>Autodesk</td>
               <td>5/28/2019</td>
               <td>amc</td>
+            </tr>  
+            <tr>
+              <td><Link to="/results/CRM">CRM</Link></td>
+              <td>SalesForce</td>
+              <td>6/4/2019</td>
+              <td>amc</td>
+            </tr>  
+            <tr>
+              <td><Link to="/results/TGT">TGT</Link></td>
+              <td>Target</td>
+              <td>6/5/2019</td>
+              <td>bmo</td>
             </tr>  
           </tbody>            
        </table><br/>
