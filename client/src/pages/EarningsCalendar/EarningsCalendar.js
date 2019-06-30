031365b1b4a89d3646ffefe6314fdd1069118763
Anthony <ar3home@gmail.com>
2019-06-29 20:58:55 -0700

    update

--- client/src/pages/EarningsCalendar/EarningsCalendar.js
+++ client/src/pages/EarningsCalendar/EarningsCalendar.js
@@ -11,7 +11,7 @@ class EarningsCalendar extends Component {
     return (
       <div className="tableContainer"> 
         <h4>EarningStats Watchlist Earnings Calendar</h4> 
-        <p style="text-align:right;">* earnings date not confirmed by company yet</p>
+        <p>* earnings date not confirmed by company yet</p>
 
 
 
