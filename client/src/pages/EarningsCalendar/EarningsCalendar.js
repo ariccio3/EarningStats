581b8ba4e4991dea7f19c696e0ec2ad731804e98
Anthony <ar3home@gmail.com>
2019-08-30 07:05:10 -0700

    Update

--- client/src/pages/EarningsCalendar/EarningsCalendar.js
+++ client/src/pages/EarningsCalendar/EarningsCalendar.js
@@ -16,26 +16,6 @@ class EarningsCalendar extends Component {
 
 
 
-        <table className="table table-striped table-hover table-bordered">
-          <thead>
-            <tr>
-              <th className="stock">Stock</th>
-              <th className="company">Company</th>
-              <th className="date">Date</th>
-              <th className="time">Time</th>
-            </tr>        
-          </thead>
-          <tbody>      
-            <tr>
-              <td><Link to="/results/ULTA">ULTA</Link></td>
-              <td>Ulta Beauty</td>
-              <td>8/29/2019</td>
-              <td>amc</td>
-            </tr>      
-          </tbody>
-        </table>
-
-
 
         <table className="table table-striped table-hover table-bordered">
           <thead>
@@ -450,7 +430,13 @@ class EarningsCalendar extends Component {
               <td>Autodesk</td>
               <td>11/27/2019</td>
               <td>amc</td>
-            </tr>  
+            </tr>    
+            <tr>
+              <td><Link to="/results/ULTA">ULTA</Link></td>
+              <td>Ulta Beauty</td>
+              <td>11/29/2019</td>
+              <td>amc</td>
+            </tr>   
           </tbody>
         </table>
 
