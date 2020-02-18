67edfa448f69b3061dbe31f3ff754dd5e88e730a
Anthony <ar3home@gmail.com>
2020-02-18 06:44:37 -0800

    Update

--- client/src/pages/EarningsCalendar/EarningsCalendar.js
+++ client/src/pages/EarningsCalendar/EarningsCalendar.js
@@ -16,26 +16,6 @@ class EarningsCalendar extends Component {
 
 
 
-        <table className="table table-striped table-hover table-bordered">
-          <thead>
-            <tr>
-              <th className="stock">Stock</th>
-              <th className="company">Company</th>
-              <th className="date">Date</th>
-              <th className="time">Time</th>
-            </tr>        
-          </thead>
-          <tbody>       
-            <tr>
-              <td><Link to="/results/WMT">WMT</Link></td>
-              <td>Walmart</td>
-              <td>2/18/2020</td>
-              <td>bmo</td>
-            </tr>      
-          </tbody>
-        </table> 
-
-
 
 
         <table className="table table-striped table-hover table-bordered">
