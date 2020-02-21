d41d09360947bece2a556845ca2746b6be0a9a86
Anthony <ar3home@gmail.com>
2020-02-21 06:52:49 -0800

    Update

--- client/src/pages/EarningsCalendar/EarningsCalendar.js
+++ client/src/pages/EarningsCalendar/EarningsCalendar.js
@@ -18,27 +18,6 @@ class EarningsCalendar extends Component {
 
 
 
-        <table className="table table-striped table-hover table-bordered">
-          <thead>
-            <tr>
-              <th className="stock">Stock</th>
-              <th className="company">Company</th>
-              <th className="date">Date</th>
-              <th className="time">Time</th>
-            </tr>        
-          </thead>
-          <tbody>    
-            <tr>
-              <td><Link to="/results/DE">DE</Link></td>
-              <td>Deere &amp; Company</td>
-              <td>2/21/2020</td>
-              <td>bmo</td>
-            </tr>     
-          </tbody>
-        </table>
-
-
-
 
 
         <table className="table table-striped table-hover table-bordered">
@@ -509,7 +488,13 @@ class EarningsCalendar extends Component {
             <tr>
               <td><Link to="/results/DPZ">DPZ</Link></td>
               <td>Dominos Pizza</td>
-              <td>2/20/2020</td>
+              <td>5/20/2020</td>
+              <td>bmo</td>
+            </tr>  
+            <tr>
+              <td><Link to="/results/DE">DE</Link></td>
+              <td>Deere &amp; Company</td>
+              <td>5/21/2020</td>
               <td>bmo</td>
             </tr>  
           </tbody>
