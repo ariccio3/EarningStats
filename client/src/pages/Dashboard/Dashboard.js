c105f1661486102677b10091b8cc8203b337638a
Anthony <ar3home@gmail.com>
2020-07-19 10:19:13 -0700

    Update

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -6,9 +6,13 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="container">
-        <iframe width="933" height="700" 
+/*        <iframe width="933" height="700" 
           src="https://app.powerbi.com/view?r=eyJrIjoiYTVkOWQxOWQtMDI3YS00NmUwLWI1YjktNTJlMTI3MzY2NjlmIiwidCI6ImIwMGE1ZDU0LTI3MzAtNDA2OC04MGIwLTgxNTMwYjhhNDkwZiIsImMiOjJ9"
           frameborder="0" allowFullScreen="true">  
+        </iframe>*/
+        <iframe width="933" height="700"
+         src="https://app.powerbi.com/view?r=eyJrIjoiNzQ5ZjgyZTItNDc0MS00NDRjLThiOGYtODJlODU5MDk2YjAxIiwidCI6IjFiYjBiNTBjLWNlNDktNGVhNy05NzRjLTdhZmI4NmYzOGIwZCIsImMiOjZ9"
+         frameborder="0" allowFullScreen="true">
         </iframe>
       </div>
     )  
