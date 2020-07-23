a2aeaa2cfef9ba24c482b0f9dd178ec7436629e4
Anthony <ar3home@gmail.com>
2020-07-23 07:06:19 -0700

    Update

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import API from "../../utils/API";
+//import API from "../../utils/API";
 import "./Dashboard.css";
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="container">
-        <iframe width="933" height="700"
+        <iframe width="933" height="700" title="Dashboard"
          src="https://app.powerbi.com/view?r=eyJrIjoiNzFiYzU1NzMtMTY0Ny00OTBlLTgwMmYtZWQxODA4ODAzOTIyIiwidCI6IjFiYjBiNTBjLWNlNDktNGVhNy05NzRjLTdhZmI4NmYzOGIwZCIsImMiOjZ9" 
          frameborder="0" allowFullScreen="true">
         </iframe>
