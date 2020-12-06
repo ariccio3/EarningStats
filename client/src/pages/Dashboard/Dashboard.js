d73da51f921dbd181920942139cf1e8022040d44
Anthony <ar3home@gmail.com>
2020-12-06 08:09:47 -0800

    Update

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -5,11 +5,12 @@ import "./Dashboard.css";
 class Dashboard extends Component {
   render() {
     return (
+
       <div className="iframe-container">
         <iframe 
           class="responsive-iframe"
           title="Dashboard"
-          src="https://app.powerbi.com/view?r=eyJrIjoiNzFiYzU1NzMtMTY0Ny00OTBlLTgwMmYtZWQxODA4ODAzOTIyIiwidCI6IjFiYjBiNTBjLWNlNDktNGVhNy05NzRjLTdhZmI4NmYzOGIwZCIsImMiOjZ9"  
+          src="https://app.powerbi.com/view?r=eyJrIjoiNzFiYzU1NzMtMTY0Ny00OTBlLTgwMmYtZWQxODA4ODAzOTIyIiwidCI6IjFiYjBiNTBjLWNlNDktNGVhNy05NzRjLTdhZmI4NmYzOGIwZCIsImMiOjZ9&pageName=ReportSection81245537e5e5153c07c9"  
           frameborder="0" 
           allowFullScreen="true">
         </iframe>
